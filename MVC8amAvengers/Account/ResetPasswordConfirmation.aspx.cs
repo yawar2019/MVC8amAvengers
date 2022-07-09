a0f5b40c264b1c7eb29fafca12cf2a6060b5f3bc
yawar2019 <yawarali17@gmail.com>
2022-07-09 08:43:30 +0530

    Add project files.

---
+++ MVC8amAvengers/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MVC8amAvengers.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MVC8amAvengers/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC8amAvengers.Startup))]
+namespace MVC8amAvengers
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
