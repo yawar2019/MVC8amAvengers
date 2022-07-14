0d65dc215914c087d646aed40e61090b5d497e86
yawar2019 <yawarali17@gmail.com>
2022-07-14 21:12:19 +0530

    committed

---
+++ Mvc8amAvengersTeam/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mvc8amAvengersTeam
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+               name: "xyz",
+               url: "pista/house",
+               defaults: new { controller = "new", action = "GetMeView", eid = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+    name: "Default",
+    url: "{controller}/{action}/{id}",
+    defaults: new { controller = "new", action = "Index", id = UrlParameter.Optional }
+);
+
+
+
+
+        }
+    }
+}
---
+++ Mvc8amAvengersTeam/Controllers/NewController.cs
@@ -0,0 +1,118 @@
+﻿using Mvc8amAvengersTeam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc8amAvengersTeam.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        //[NonAction]
+        public string Index()
+        {
+            return "hello";
+        }
+
+        public ActionResult GetMeView()
+        {
+            string a = Index();
+            ViewBag.info =a;
+            return View();
+        }
+        //controller/action/eid
+        [Route("student/Arpita")]
+        [Route("student/priya")]
+        [Route("new/test")]
+        public string test(int? id)
+        {
+            return "My Employee id is "+id+" Name is "+Request.QueryString["name"]+ "Designation is "+Request.QueryString["Designation"];
+        }
+
+        public ActionResult getSomeData()
+        {
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+
+            ViewBag.empinfo = emp;
+
+            return View();
+        }
+
+        public ActionResult getSomeData2()
+        {
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Johny";
+            emp1.EmpSalary = 900000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "James";
+            emp2.EmpSalary = 600000;
+
+            listEmployee.Add(emp);
+            listEmployee.Add(emp1);
+            listEmployee.Add(emp2);
+
+            ViewBag.Emp = listEmployee;
+
+
+            return View();
+        }
+
+        public ActionResult getSomeData3()
+        {
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+            string a = "asdf";
+            String b = "asdfs";
+
+            //object model = emp;
+            return View(emp);
+        }
+
+        public ActionResult getSomeData4()
+        {
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Johny";
+            emp1.EmpSalary = 900000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "James";
+            emp2.EmpSalary = 600000;
+
+            listEmployee.Add(emp);
+            listEmployee.Add(emp1);
+            listEmployee.Add(emp2);
+
+           
+
+
+            return View(listEmployee);
+        }
+
+    }
+}
