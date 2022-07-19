2db70ec68948d87ac80b962d66cc4b281033433b
yawar2019 <yawarali17@gmail.com>
2022-07-19 08:53:42 +0530

    committed

---
+++ Mvc8amAvengersTeam/Models/EmpDept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc8amAvengersTeam.Models
+{
+    public class EmpDept
+    {
+        public List<EmployeeModel> emp { get; set; }
+        public List<DepartmentModel> dept { get; set; }
+    }
+}
