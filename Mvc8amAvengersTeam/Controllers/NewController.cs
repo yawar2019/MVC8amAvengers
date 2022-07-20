ff545b1f1b9af1ab64df4dd4784f551444e58f05
yawar2019 <yawarali17@gmail.com>
2022-07-20 08:52:02 +0530

    commited

--- Mvc8amAvengersTeam/Controllers/NewController.cs
+++ Mvc8amAvengersTeam/Controllers/NewController.cs
@@ -9,7 +9,7 @@ namespace Mvc8amAvengersTeam.Controllers
 {
     public class NewController : Controller
     {
-        // GET: New Madhavi
+        // GET: New javeed
         //[NonAction]
         public string Index()
         {
