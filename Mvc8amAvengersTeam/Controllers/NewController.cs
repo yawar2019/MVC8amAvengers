6630c3b0bdc7feac85dca8f3e784ed57427403de
yawar2019 <yawarali17@gmail.com>
2022-07-27 09:07:38 +0530

    committed

--- Mvc8amAvengersTeam/Controllers/NewController.cs
+++ Mvc8amAvengersTeam/Controllers/NewController.cs
@@ -267,5 +267,97 @@ namespace Mvc8amAvengersTeam.Controllers
 
             return RedirectToAction("Index2", "Default", listEmployee);
         }
+
+        public EmptyResult ExecuteLogic()
+        {
+            ///logic 
+            return new EmptyResult();
+        }
+
+        public JsonResult GetjsonData()
+        {
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Johny";
+            emp1.EmpSalary = 900000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "James";
+            emp2.EmpSalary = 600000;
+
+            listEmployee.Add(emp);
+            listEmployee.Add(emp1);
+            listEmployee.Add(emp2);
+
+
+            DepartmentModel dept1 = new DepartmentModel();
+            dept1.DeptId = 1211;
+            dept1.DeptName = "IT";
+
+            DepartmentModel dept2 = new DepartmentModel();
+            dept2.DeptId = 1212;
+            dept2.DeptName = "Marketing";
+
+            List<DepartmentModel> listDept = new List<Models.DepartmentModel>();
+            listDept.Add(dept1);
+            listDept.Add(dept2);
+
+
+            EmpDept ed = new EmpDept();
+            ed.emp = listEmployee;
+            ed.dept = listDept;
+            return Json(ed, JsonRequestBehavior.AllowGet);
+        }
+
+        
+        public JsonResult GetjsonData2()
+        {
+            List<EmployeeModel> listEmployee = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Radha";
+            emp.EmpSalary = 700000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Johny";
+            emp1.EmpSalary = 900000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "James";
+            emp2.EmpSalary = 600000;
+
+            listEmployee.Add(emp);
+            listEmployee.Add(emp1);
+            listEmployee.Add(emp2);
+
+
+ 
+            return Json(listEmployee, JsonRequestBehavior.AllowGet);
+        }
+
+        public ViewResult GetjsonResultInView()
+        {
+            return View();
+        }
+
+        public FileResult GetFileData()
+        {
+            return File("~/Web.config","text");
+        }
+        public FileResult GetFileData2()
+        {
+            return File("~/Web.config", "application/xml");
+        }
     }
 }
