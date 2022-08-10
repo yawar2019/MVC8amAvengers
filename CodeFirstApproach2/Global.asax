4aa55891f5fc140f8787b296cd01cd4927e21401
yawar2019 <yawarali17@gmail.com>
2022-08-10 09:01:46 +0530

    Committed

---
+++ CodeFirstApproach2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeFirstApproach2.MvcApplication" Language="C#" %>
---
+++ DatabaseApproach2/Models/EmpDept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseApproach2.Models
+{
+    public class EmpDept
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+    }
+}
