d6e8666d5543aeb29fd1101e4346b05c44b5c42c
yawar2019 <yawarali17@gmail.com>
2022-08-12 09:13:56 +0530

    Commit

--- CodeFirstApproach2/Controllers/HomeController.cs
+++ CodeFirstApproach2/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using CodeFirstApproach2.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,5 +17,64 @@ namespace CodeFirstApproach2.Controllers
         {
             return View(db.EmployeeModels.ToList());
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//Genereate Insert Query
+            int i = db.SaveChanges();//ExecuteNonQuery Method will retun no's of rows effeted return int 
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State=EntityState.Modified;//Genereate Update Query
+            int i = db.SaveChanges();//ExecuteNonQuery Method will retun no's of rows effeted return int 
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            db.EmployeeModels.Remove(emp);
+            int i = db.SaveChanges();//ExecuteNonQuery Method will retun no's of rows effeted return int 
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+
     }
 }
