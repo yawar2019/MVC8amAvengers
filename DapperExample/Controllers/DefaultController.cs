ece10340bb67c46ddce75422dbd2f87ed026be07
yawar2019 <yawarali17@gmail.com>
2022-08-29 08:58:51 +0530

    Committed

--- DapperExample/Controllers/DefaultController.cs
+++ DapperExample/Controllers/DefaultController.cs
@@ -25,7 +25,45 @@ namespace DapperExample.Controllers
         public ActionResult Create(EmployeeModel emp)
         {
             int i = db.SaveEmployee(emp);
+
             if(i>0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var Emp = db.GetEmployeeById(id);
+            return View(Emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            int i = db.UpdateEmployee(emp);
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var Emp = db.GetEmployeeById(id);
+            return View(Emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            int i = db.DeleteEmployee(id);
+
+            if (i > 0)
             {
                 return RedirectToAction("Index");
             }
--- DapperExample/Models/EmployeeContext.cs
+++ DapperExample/Models/EmployeeContext.cs
@@ -29,5 +29,33 @@ namespace DapperExample.Models
             var result = con.Execute("sp_CreateEmployee",param:parameter,commandType: CommandType.StoredProcedure);
             return result;
         }
+
+        public EmployeeModel GetEmployeeById(int? id)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@empId", id);
+            var result = con.QuerySingle<EmployeeModel>("spr_getEmployeeDetailsbyId", param: parameter, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+
+        public int UpdateEmployee(EmployeeModel emp)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@Empid", emp.EmpId);
+            parameter.Add("@EmpName", emp.EmpName);
+            parameter.Add("@EmpSalary", emp.EmpSalary);
+
+            var result = con.Execute("spr_updateEmployeeDetails", param: parameter, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+
+        public int DeleteEmployee(int? id)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@Empid", id);
+            
+            var result = con.Execute("usp_DeleteEmployeeById", param: parameter, commandType: CommandType.StoredProcedure);
+            return result;
+        }
     }
 }
